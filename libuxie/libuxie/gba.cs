34496cf1001d4492c7b0c8cf9e4724df17e45647
Chase <chasesan@gmail.com>
2014-10-24 20:58:52 -0400

    Trimming GBA

--- libuxie/libuxie/gba.cs
+++ libuxie/libuxie/gba.cs
@@ -32,16 +32,13 @@ namespace LibUxie.GBA {
 		private const int BLOCK_COUNT = 14;
 		private const int BLOCK_LENGTH = 0x1000;
 		private const int UNPACKED_BLOCK_LENGTH = 0xF80;
-		private const int FOOTER_LENGTH = 0xC;
 		private const uint FOOTER_MARK = 0x08012025;
 		private const int UNPACKED_SIZE = 0xD900;
 		private const int PACKED_SIZE = 0x20000;
 		private const int SAVE_SECTION = 0xE000;
 
 		private const int RSE_SECKEY_OFFSET = 0xAC;
-		private const int RSE_SECKEY2_OFFSET = 0x1F4;
 		private const int FRLG_SECKEY_OFFSET = 0xAF8;
-		private const int FRLG_SECKEY2_OFFSET = 0xF20;
 
 		private const int RSE_STORAGE = UNPACKED_BLOCK_LENGTH + 0x490;
 		private const int FRLG_STORAGE = UNPACKED_BLOCK_LENGTH + 0x290;
@@ -59,6 +56,7 @@ namespace LibUxie.GBA {
 
 		/* Unsafe a.k.a. "I got this." */
 		private static unsafe Footer *GetFooter(byte[] data, int offset, int block) {
+			const int FOOTER_LENGTH = 0xC;
 			offset = offset + ((block + 1) * BLOCK_LENGTH - FOOTER_LENGTH);
 			fixed(byte* ptr = &data[offset]) {
 				return (Footer*)ptr;
@@ -79,36 +77,33 @@ namespace LibUxie.GBA {
 			}
 			return 0;
 		}
-
-		private unsafe bool IsValid(byte[] data) {
+		private unsafe static Version DetectVersion(byte[] data) {
 			if(data.Length != PACKED_SIZE) {
-				return false;
+				return Version.Unknown;
 			}
 			uint mark = GetFooter(data, 0, 0)->mark;
 			if(mark != FOOTER_MARK) {
-				return false;
-			}
-			return true;
-		}
-
-		private Version DetectVersion() {
-			/* Detecting GBA version is a real pain. */
-			/* Ruby/Sapphire */
-			if(BitConverter.ToInt32(unpacked, RSE_SECKEY_OFFSET) == 0
-			&& BitConverter.ToInt32(unpacked, RSE_SECKEY2_OFFSET) == 0) {
-				return Version.RubySapphire;
+				return Version.Unknown;
 			}
 
-			/* Emerald */
-			if(BitConverter.ToInt32(unpacked, RSE_SECKEY_OFFSET)
-			== BitConverter.ToInt32(unpacked, RSE_SECKEY2_OFFSET)) {
-				return Version.Emerald;
-			}
+			/* These are all in the first block. So no need to unpack first. */
+			const int RSE_SECKEY2_OFFSET = 0x1F4;
+			const int FRLG_SECKEY2_OFFSET = 0xF20;
 
 			/* FireRed/LeafGreen */
-			if(BitConverter.ToInt32(unpacked, FRLG_SECKEY_OFFSET)
-			== BitConverter.ToInt32(unpacked, FRLG_SECKEY2_OFFSET)) {
+			if(BitConverter.ToInt32(data, FRLG_SECKEY_OFFSET)
+			== BitConverter.ToInt32(data, FRLG_SECKEY2_OFFSET)) {
 				return Version.FireRedLeafGreen;
+			} else
+			/* Emerald */
+				if(BitConverter.ToInt32(data, RSE_SECKEY_OFFSET)
+			== BitConverter.ToInt32(data, RSE_SECKEY2_OFFSET)) {
+				return Version.Emerald;
+			}
+			/* Ruby/Sapphire */
+			if(BitConverter.ToInt32(data, RSE_SECKEY_OFFSET) == 0
+			&& BitConverter.ToInt32(data, RSE_SECKEY2_OFFSET) == 0) {
+				return Version.RubySapphire;
 			}
 
 			return Version.Unknown;
@@ -147,10 +142,11 @@ namespace LibUxie.GBA {
 		}
 
 		public bool Load(byte[] data, SaveSlot slot) {
-			if(!IsValid(data)) {
-				type = Version.Unknown;
+			type = DetectVersion(data);
+			if(Version.Unknown == type) {
 				return false;
 			}
+
 			int offset = GetTypeOffset(data, slot);
 			order = new byte[BLOCK_COUNT];
 			unpacked = new byte[UNPACKED_SIZE];
@@ -165,13 +161,11 @@ namespace LibUxie.GBA {
 				unsafe {
 					order[i] = (byte)GetFooter(data, offset, i)->sectionId;
 				}
-
 				Array.Copy(
 					data, offset + i * BLOCK_LENGTH,
 					unpacked, order[i] * UNPACKED_BLOCK_LENGTH,
 					UNPACKED_BLOCK_LENGTH);
 			}
-			type = DetectVersion();
 			Crypt();
 
 			return true;
